2aaa388ccfe3b1e37da0d8ff69a341f4a6509c08
andrew <andrew.oxenburgh@gmail.com>
2017-09-24 11:40:45 +1300

    implemented

---
+++ index.js
@@ -0,0 +1,24 @@
+/*global exports */
+
+let notValid = (number) => (typeof number === 'undefined'
+    || number === null
+    || number < 0
+    || (number.trim && number.trim() === '')
+    || (typeof number === 'number' && !Number.isInteger(number))
+);
+
+exports.factorial = function factorial(number) {
+    if (notValid(number)) {
+        return undefined;
+    }
+    if (number == 0) {
+        return 1;
+    }
+
+    let sum = 1;
+    for (let i = 1; i <= number; i++) {
+        sum *= i;
+    }
+    return sum;
+};
+
--- test/factorial.test.js
+++ test/factorial.test.js
@@ -1,18 +1,52 @@
-
 /*global require describe it */
-var assert = require('assert');
+let assert = require('assert');
+let factorial = require('../index.js').factorial;
 
-function factorial(number) {
-    return 1;
-}
 
-describe('Factorial', function() {
-    describe('good cases', function() {
-        it('factorial 1 should be 1', function() {
-            assert.equal(1, factorial(1));
+describe('Factorial', function () {
+    describe('should accept string reps of numbers', function () {
+        it('factorial 0 should be 1', function () {
+            assert.equal(factorial('1'), 1);
         });
-        it('factorial 0 should be 1', function() {
-            assert.equal(1, factorial(1));
+    });
+    describe('should return undefined on error', function () {
+        it('factorial -1', function () {
+            assert.equal(factorial(-1), undefined);
+        });
+        it('factorial only works on integers', function () {
+            assert.equal(factorial(1.1), undefined);
+        });
+        it('factorial undefined', function () {
+            assert.equal(factorial(), undefined);
+        });
+        it('factorial undefined', function () {
+            assert.equal(factorial(undefined), undefined);
+        });
+        it('factorial null', function () {
+            assert.equal(factorial(null), undefined);
+        });
+        it('factorial empty string', function () {
+            assert.equal(factorial(''), undefined);
+        });
+    });
+    describe('good cases', function () {
+        it('factorial 0 should be 1', function () {
+            assert.equal(factorial(1), 1);
+        });
+        it('factorial 1 should be 1', function () {
+            assert.equal(factorial(1), 1);
+        });
+        it('factorial 2 should be 2', function () {
+            assert.equal(factorial(2), 2);
+        });
+        it('factorial 3 should be 6', function () {
+            assert.equal(factorial(3), 6);
+        });
+        it('factorial "3" should be 6', function () {
+            assert.equal(factorial('3'), 6);
+        });
+        it('factorial 10 should be 36282800', function () {
+            assert.equal(factorial('3'), 6);
         });
     });
 });
