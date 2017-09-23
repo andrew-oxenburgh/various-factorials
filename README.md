c6264aff056f26add2ab4eb5cb0ca62984d7a206
andrew <andrew.oxenburgh@gmail.com>
2017-09-24 11:04:20 +1300

    initial tests

---
+++ README.md
@@ -0,0 +1,5 @@
+## Various factorials
+
+This project is for me to try out a few ways of doing the same thing, ie: factorial
+
+Initially it'll be in JS, but then maybe in Python, and other languages.
--- test/factorial.test.js
+++ test/factorial.test.js
@@ -1,10 +1,18 @@
 
 /*global require describe it */
 var assert = require('assert');
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal(1, [1,2,3].indexOf(2));
+
+function factorial(number) {
+    return 1;
+}
+
+describe('Factorial', function() {
+    describe('good cases', function() {
+        it('factorial 1 should be 1', function() {
+            assert.equal(1, factorial(1));
+        });
+        it('factorial 0 should be 1', function() {
+            assert.equal(1, factorial(1));
         });
     });
 });
